c2b8f0644592ce620125fb9d70d57aebf80bfc55
Xerksez <91895432+Xerksez@users.noreply.github.com>
2021-12-13 15:21:41 +0100

    309

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,56 @@
+#include <stdio.h>
+#include <array>
 #include <iostream>
-auto sortuj(int t[], int n) -> void
+#include <string>
+auto zamiana(int* a, int* b) -> void
 {
-    for(int i=0; i<n; i++)
-    {
-        for(int j=1; j<n-i; j++)
-            if(t[j-1]<t[j])
-            {
-                int tym = t[j];
-                t[j] = t[j-1];
-                t[j-1] = tym;
-            }
-    }
-    for(int i=0; i<n; i++)
-        std::cout<<t[i]<<"\n";
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
+auto srodek(int tab[], int nisko, int wysoko) -> int
+{
+    int more = tab[wysoko];
+    int i = (nisko - 1);
+    for (int j = nisko; j <= wysoko - 1; j++)
+    {
+        if (tab[j] <= more)
+        {
+            i++;
+            zamiana(&tab[i], &tab[j]);
+        }
+    }
+    zamiana(&tab[i + 1], &tab[wysoko]);
+    return (i + 1);
+
+}
+
+
+void sortowanie(int tab[], int nisko, int wysoko)
+{
+    if (nisko < wysoko)
+    {
+        int p = srodek(tab, nisko, wysoko);
+        sortowanie(tab, nisko, p - 1);
+        sortowanie(tab, p + 1, wysoko);
+    }
+}
+
+
+void wypisanie(int tab[], int wielkosc)
+{
+    int i;
+    for (i = 0; i < wielkosc; i++)
+        std::cout << tab[i]<<" ";
+}
+
 
 auto main() -> int
 {
-    int tablica[] = { 42, 9, -1, 18, 59, 3, 101, 31, 72, 12 };
-    sortuj(tablica,10);
+    int tab[] = {42, 9, -1, 18, 59, 3, 101, 31, 72, 12};
+    int n = sizeof(tab)/sizeof(tab[0]);
+    sortowanie(tab, 0, n - 1);
+    std::cout << "Tablica posortowana poprawnie\n-> ";
+    wypisanie(tab, n);
     return 0;
 }
