b5fd6a39057129d1fec59b13c363c8ada0dde733
Xerksez <91895432+Xerksez@users.noreply.github.com>
2022-01-09 19:08:26 +0100

    306
    306

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <string>
 
-auto amax(int t[], int n) -> int
+auto pytanie(std::string tekst) -> int
 {
-
-    int maksymalna = t[0];
-    for(int i = 1;i < n; ++i){
-       if(maksymalna < t[i])
-           maksymalna = t[i];
-    }
-
-
-    std::cout << "Najwieksza liczba to  " << maksymalna;
-
-    return maksymalna;
+    int n;
+    std::cout << tekst << "\n";
+    std::cin >> n;
+    return n;
 }
-
-
+auto search(int a[], int n, int szukana) -> int
+{
+    for(int i=0;i<n;i++){
+        if(szukana == a[i]){
+            std::cout << "ta liczba jest "<<i+1<<" w tabeli";
+            return i;
+        }
+        }
+        std::cout << "Nie ma takiej liczby";
+        return -1;
+    }
 auto main() -> int
 {
+	int tab[10] { 42 , 9 , -1 , 18 ,3 , 59 , 101 , 31 , 72 , 12 };
 
-	int tablica[10] { 42 , 9 , -1 , 18 , 59, 3, 101 , 31 , 72 , 12,} ;
-    amax(tablica,10);
+    auto const c = pytanie ("Jakiej liczby szukasz?");
+    int s = search(tab,10,c);
+
+
 return 0;
 }
